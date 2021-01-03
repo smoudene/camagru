e66e08f7db04b35b8165ac97b5290d8c066b1390
smoudene <smoudene25@gmail.com>
2021-01-03 21:25:58 +0100

    verif mail/modif profile

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -5,6 +5,8 @@
         public function __construct()
         {
             $this->userModel = $this->model('User');
+            $this->postModel = $this->model('Post');
+
         }
 
         public function signup() {
@@ -12,13 +14,15 @@
             {
 
                 $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
+                $token = openssl_random_pseudo_bytes(16);
+                $token = bin2hex($token);
                 $data = [
                     'fullname' => trim($_POST['fullname']),
                     'email' => trim($_POST['email']),
                     'username' => trim($_POST['username']),
                     'password' => trim($_POST['password']),
                     'confirm_pwd' => trim($_POST['confirmPwd']),
+                    'token' => $token,
                     'err_fullname' => '',
                     'err_email' => '',
                     'err_username' => '',
@@ -44,6 +48,14 @@
                 }
                 if (empty($data['password']))
                     $data['err_password'] = 'please enter password !!';
+                else if (strlen($data['password']) < 6)
+                    $data['err_password'] = 'Password must be at least 6 characters';
+                else if (!preg_match('@[A-Z]@', $data['password']))
+                    $data['err_password'] = 'Password must contain an upper case';
+                else if (!preg_match('@[a-z]@', $data['password']))
+                    $data['err_password'] = 'Password must contain a  lower case';
+                else if (!preg_match('@[0-9]@', $data['password']))
+                    $data['err_password'] = 'Password must contain a number';
                 if ($data['password'] != $data['confirm_pwd'])
                     $data['err_confirmPwd'] = 'Passwords do not match !!';
 
@@ -53,8 +65,26 @@
                     $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
                     if ($this->userModel->signup($data))
                     {
-                        pop_up('signup_ok', 'You are now part of our community, you can login now');
-                        redirect('users/login');
+                        $to_email = $data['email'];
+                        $subject = "Verify you email";
+                        $token = $data['token'];
+                        $body = '<p><h1>Welcome to Camagru</h1>,
+                            <br /><br />
+                            <br/>
+                            To verify your account click here 
+                            <a href="http://localhost/Camagru/users/verification/?token='.$token.'">click here.</a>
+                            </p>
+                            <p>
+                                <br />--------------------------------------------------------
+                                <br />This is an automatic mail , please do not reply.
+                            </p>';
+                        $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
+                        $headers .= 'From: <oes-safi@Camagru.ma>' . "\r\n";    
+                        if (mail($to_email, $subject, $body, $headers))
+                                pop_up('signup_ok', 'You are now part of our community, you can login now');
+                            else
+                                pop_up('signup_ok', 'Can not send emai verificaton, please retry');
+                            redirect('users/login');   
                     }
                     else
                         die('wrong');
@@ -70,6 +100,7 @@
                     'username' => '',
                     'password' => '',
                     'confirm_pwd' => '',
+                    'token' => '',
                     'err_name' => '',
                     'err_email' => '',
                     'err_username' => '',
@@ -105,7 +136,13 @@
                     $loggedUser = $this->userModel->login($data['username'], $data['password']);
                     if ($loggedUser)
                     {
-                        $this->createUserSession($loggedUser);
+                        if($loggedUser->verified)
+                            $this->createUserSession($loggedUser);
+                        else
+                        {
+                            pop_up('not_verified', 'Please verify you email !!', 'alert alert-danger');
+                            redirect('users/login');
+                        }
                     }
                     else
                     {
@@ -150,10 +187,28 @@
 
             redirect('posts');
         }
+
+        public function verification()
+        {
+            if (isset($_GET['token']))
+            {
+                $token = $_GET['token'];
+                
+                if ($this->userModel->verify($token))
+                {
+                    pop_up('signup_ok', 'Your account is verified succesfully');
+                    redirect('users/login');
+                }
+            }
+            else
+                die('error');
+        }
         
         public function profile() {
+            $post = $this->postModel->getPosts();
             $data = [
-                'username' => $_SESSION['username']
+                'username' => $_SESSION['username'],
+                'posts' =>$post
             ];
             
             $this->view('users/profile', $data);
@@ -164,11 +219,53 @@
             $data = [
                 'id' => $_SESSION['user_id'],
             ];
-            if($this->userModel->update_username($_POST['new_username'], $data['id']))
+            if(!empty($_POST['new_username']))
             {
-                pop_up('updated', 'Profile updated ✓', 'pop alert alert-success w-50 mx-auto text-center');
-                $_SESSION['user_username'] = $_POST['new_username'];
-                redirect('users/profile');
+                if($this->userModel->update_username($_POST['new_username'], $data['id']))
+                {
+                    pop_up('updated', 'Username updated ✓', 'pop alert alert-success w-50 mx-auto text-center');
+                    $_SESSION['user_username'] = $_POST['new_username'];
+                    redirect('users/profile');
+                }
             }
+            else
+                redirect('users/profile');
+
+            if(!empty($_POST['new_fullname']))
+            {
+                if($this->userModel->update_fullname($_POST['new_fullname'], $data['id']))
+                {
+                    pop_up('updated', 'Fullname updated ✓', 'pop alert alert-success w-50 mx-auto text-center');
+                    $_SESSION['user_fullname'] = $_POST['new_fullname'];
+                    redirect('users/profile');
+                }
+            }
+            else
+                redirect('users/profile');
+            
+            if(!empty($_POST['new_email']))
+            {
+                if($this->userModel->update_email($_POST['new_email'], $data['id']))
+                {
+                    pop_up('updated', 'You need to verify the new email ✓', 'pop alert alert-success w-50 mx-auto text-center');
+                    $_SESSION['user_fullname'] = $_POST['new_fullname'];
+                    redirect('users/profile');
+                }
+            }
+            else
+                redirect('users/profile');
+            
+            if(!empty($_POST['new_password']))
+            {
+                $_POST['new_password'] = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
+                if($this->userModel->update_pass($_POST['new_password'], $data['id']))
+                {
+                    pop_up('updated', 'Password updated ✓', 'pop alert alert-success w-50 mx-auto text-center');
+                    redirect('users/profile');
+                }
+            }
+            else
+                redirect('users/profile');
+
         }
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,13 @@
 
 
         public function signup($data)
-        {
-            $this->db->query('INSERT INTO users (fullname, email, username, password) VALUES(:fullname, :email, :username, :password)');
+        {   
+            $this->db->query('INSERT INTO users (fullname, email, username, password, token) VALUES(:fullname, :email, :username, :password, :token)');
             $this->db->bind(':fullname', $data['fullname']);
             $this->db->bind(':email', $data['email']);
             $this->db->bind(':username', $data['username']);
             $this->db->bind(':password', $data['password']);
+            $this->db->bind(':token', $data['token']);
             
             if ($this->db->execute())
                 return true;
@@ -34,6 +35,27 @@
                 return $row;
             else
                 return false;
+        }
+
+        public function verify($token)
+        {
+            $this->db->query('SELECT * FROM users WHERE token = :token');
+            $this->db->bind(':token', $token);
+            
+            $row = $this->db->singleFetch();
+
+
+        if ($this->db->rowCount() > 0)
+        {
+            $this->db->query('UPDATE users SET verified = 1 WHERE token = :token');
+            $this->db->bind(':token', $token);
+            if ($this->db->execute())
+                return true;
+            else
+               return false;
+        }
+        else
+            return false;
         }
 
         public function findUsrByEmail($email){
@@ -66,7 +88,43 @@
 
             $this->db->query('UPDATE users SET username = :username WHERE id = :id');
             $this->db->bind(':username', $new_username);
-            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':id', $data);
+
+            if ($this->db->execute())
+                return true;
+            else
+                return false;
+        }
+
+        public function update_fullname($new_fullname, $data){
+
+            $this->db->query('UPDATE users SET fullname = :fullname WHERE id = :id');
+            $this->db->bind(':fullname', $new_fullname);
+            $this->db->bind(':id', $data);
+
+            if ($this->db->execute())
+                return true;
+            else
+                return false;
+        }
+
+        public function update_pass($new_password, $data){
+
+            $this->db->query('UPDATE users SET password = :password WHERE id = :id');
+            $this->db->bind(':password', $new_password);
+            $this->db->bind(':id', $data);
+
+            if ($this->db->execute())
+                return true;
+            else
+                return false;
+        }
+
+        public function update_email($new_email, $data){
+
+            $this->db->query('UPDATE users SET email = :email WHERE id = :id');
+            $this->db->bind(':email', $new_email);
+            $this->db->bind(':id', $data);
 
             if ($this->db->execute())
                 return true;
